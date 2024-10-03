e7263b301af14e681e9d9f0dee65a4e63f56d865
Fernando Bueno <fcbgomes@gmail.com>
2024-10-03 01:52:01 -0300

    feat: added links search functionality to ThemeCard

--- src/components/Themes/ThemeCard/index.tsx
+++ src/components/Themes/ThemeCard/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { Theme } from "../../../pages/Dashboard";
+import { Link, Theme, ThemeStatus } from "../../../pages/Dashboard";
 import api from "../../../services/api";
 import ConfirmDeleteModal from "./ModalDeleteConfirmation";
+import axios, { AxiosError } from "axios";
 
 interface ThemeCardProps {
   theme: Theme;
@@ -16,6 +17,8 @@ const ThemeCard: React.FC<ThemeCardProps> = ({
 }) => {
   const [title, setTitle] = useState<string>(theme.title);
   const [keywords, setKeywords] = useState<string>(theme.keywords);
+  const [status, setStatus] = useState<ThemeStatus>(theme.status);
+  const [links, setLinks] = useState<Link[]>(theme.links);
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
 
@@ -23,6 +26,7 @@ const ThemeCard: React.FC<ThemeCardProps> = ({
     useState<boolean>(false);
 
   const updateTheme = async () => {
+    setLoading(true);
     const payload = { title, keywords };
     try {
       const response = await api.put(`/themes/${theme.id}`, payload);
@@ -35,11 +39,39 @@ const ThemeCard: React.FC<ThemeCardProps> = ({
   };
 
   const deleteTheme = async () => {
+    setLoading(true);
     try {
       const response = await api.delete(`/themes/${theme.id}`);
       return response.data;
     } catch (err) {
       console.error("Erro na requisição.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const searchTheme = async () => {
+    setLoading(true);
+    try {
+      const response = await api.get(`/themes/${theme.id}/search-news`);
+      setStatus(ThemeStatus.COMPLETED)
+      return response.data;
+    } catch (err: unknown | AxiosError) {
+      {
+        if (axios.isAxiosError(err))  {
+          if (err.status===403){
+            setStatus(ThemeStatus.COMPLETED)
+            console.error("A pesquisa já foi feita para este Assunto!");
+          }
+          else{
+            setStatus(ThemeStatus.PENDING);
+            console.warn("Erro na requisição. Tente ajustar seus parâmetros de pesquisa novamente.");
+          }
+        } else {
+          setStatus(ThemeStatus.IN_PROGRESS)
+          console.error("Erro na requisição. Tente novamente mais tarde.");
+        }
+      }
     } finally {
       setLoading(false);
     }
@@ -53,9 +85,16 @@ const ThemeCard: React.FC<ThemeCardProps> = ({
   };
 
   const handleDelete = async () => {
-    await deleteTheme();    
+    await deleteTheme();
     onThemeDelete(theme);
     setIsConfirmationModalOpen(false);
+  };
+
+  const onThemeSearch = async () => {
+    setStatus(ThemeStatus.IN_PROGRESS);
+    const newsLinks = await searchTheme();
+    setLinks(newsLinks);
+    //timeout, and navigate to page of referred themeId.
   };
 
   return (
@@ -84,19 +123,33 @@ const ThemeCard: React.FC<ThemeCardProps> = ({
               required
             />
           </div>
-          <button type="submit">Salvar</button>
-          <button type="button" onClick={() => setIsEditing(false)}>
+          <button type="submit" disabled={loading}>
+            Salvar
+          </button>
+          <button
+            type="button"
+            onClick={() => setIsEditing(false)}
+            disabled={loading}
+          >
             Cancelar
           </button>
         </form>
       ) : (
         <>
-          <h3>{theme.title}</h3>
-          <p>Keywords: {theme.keywords}</p>
-          <p>Status: {theme.status}</p>
-          <button onClick={() => setIsEditing(true)}>Atualizar</button>
-          <button onClick={() => setIsConfirmationModalOpen(true)}>
+          <h3>{title}</h3>
+          <p>Keywords: {keywords}</p>
+          <p>Status: {status}</p>
+          <button onClick={() => setIsEditing(true)} disabled={loading}>
+            Atualizar
+          </button>
+          <button
+            onClick={() => setIsConfirmationModalOpen(true)}
+            disabled={loading}
+          >
             Deletar
+          </button>
+          <button type="button" onClick={onThemeSearch} disabled={loading}>
+            Pesquisar!
           </button>
         </>
       )}
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -7,11 +7,17 @@ export interface Link {
   id: string;
   link: URL;
 }
+
+export enum ThemeStatus {
+  PENDING = "Pending",
+  IN_PROGRESS = "In Progress",
+  COMPLETED = "Completed",
+}
 export interface Theme {
   id: string;
   title: string;
   keywords: string;
-  status: string;
+  status: ThemeStatus;
   links: Link[];
   createdAt: Date;
   updatedAt: Date;
