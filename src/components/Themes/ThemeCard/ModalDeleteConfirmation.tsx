8e33205e0f10e0f21aec9ab63282f712d84fee7b
Fernando Bueno <fcbgomes@gmail.com>
2024-10-05 09:50:35 -0300

    fix: changed ConfirmModalDeleteStyles

--- src/components/Themes/ThemeCard/ModalDeleteConfirmation.tsx
+++ src/components/Themes/ThemeCard/ModalDeleteConfirmation.tsx
@@ -12,20 +12,26 @@ const ConfirmDeleteModal: React.FC<ConfirmDeleteModalProps> = ({
 }) => {
   return (
     <div className="modal-overlay">
-      <div className="modal-content">
-        <h2 className="text-lg font-semibold mb-4">
-          Tem certeza de que deseja deletar o Assunto e todos os seus Links?
-        </h2>
-        <div className="flex justify-end modal-button">
-          <button onClick={onClose} className="cancel-button">
-            Cancelar
-          </button>
-          <button onClick={onConfirm} className="confirm-button">
-            Deletar
-          </button>
-        </div>
+    <div className="modal-content bg-gray-800 text-gray-100 p-6 rounded-lg shadow-lg">
+      <h2 className="text-lg font-semibold mb-4">
+        Tem certeza de que deseja deletar o Assunto e todos os seus Links?
+      </h2>
+      <div className="flex justify-end">
+        <button
+          onClick={onClose}
+          className="cancel-button bg-gray-600 text-gray-100 px-4 py-2 rounded mr-2 hover:bg-gray-500"
+        >
+          Cancelar
+        </button>
+        <button
+          onClick={onConfirm}
+          className="confirm-button bg-red-600 text-gray-100 px-4 py-2 rounded hover:bg-red-500"
+        >
+          Deletar
+        </button>
       </div>
     </div>
+  </div>
   );
 };
 
